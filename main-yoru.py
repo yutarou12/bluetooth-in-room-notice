18b2344d4c9e6346a6902e8d81d1b38a15205dc4
yutarou12 <sasaki.yuutarou12@gmail.com>
2025-04-26 22:30:13 +0900

    add&delete yoloを用いた画像認識に変更の為

--- main-yoru.py
+++ main-yoru.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-import time
 
 import cv2
 from ultralytics import YOLO
@@ -21,33 +20,22 @@ print(f"モデルがロードされました: {model}")
 async def detect_video(m, video_source):
     cap = cv2.VideoCapture(video_source)
     while True:
+        await asyncio.sleep(5)
         ret, frame = cap.read()
         if not ret:
             break
-
-        results = m(frame, stream=True)
+        results = m(frame)
+        pople_count = 0
         for r in results:
             boxes = r.boxes
             for box in boxes:
                 if box.cls[0] == 0:
+                    pople_count += 1
                     x1, y1, x2, y2 = box.xyxy[0]
                     cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-                    cv2.putText(frame,
-                                text='people',
-                                org=(int(x1), int(y1)),
-                                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                fontScale=0.7,
-                                color=(0, 255, 0),
-                                thickness=2,
-                                lineType=cv2.LINE_4)
 
-        cv2.imshow('Video', frame)
-        await asyncio.sleep(10)
-        if cv2.waitKey(1) == ord('q'):
-            break
-
+        print(f"人数: {pople_count}")
     cap.release()
-    cv2.destroyAllWindows()
 
 asyncio.run(detect_video(model, os.getenv('VIDEO_PATH')))
 # detect_video(model, os.getenv('VIDEO_PATH'))
