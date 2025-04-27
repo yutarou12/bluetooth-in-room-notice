62984e630993c35f323548a609597ce56f026d35
yutarou12 <sasaki.yuutarou12@gmail.com>
2025-04-27 12:12:32 +0900

    add APIにrequestを行い在室通知を実施

--- main-yoru.py
+++ main-yoru.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 
 import cv2
+import requests
 from ultralytics import YOLO
 from dotenv import load_dotenv
 load_dotenv()
@@ -18,22 +19,41 @@ print(f"モデルがロードされました: {model}")
 
 async def detect_video(m, video_source):
     cap = cv2.VideoCapture(video_source)
+    zero_count = 0
     while True:
-        await asyncio.sleep(60 * 1)
-        ret, frame = cap.read()
-        if not ret:
-            break
-        results = m(frame)
-        pople_count = 0
-        for r in results:
-            boxes = r.boxes
-            for box in boxes:
-                if box.cls[0] == 0:
-                    pople_count += 1
-                    x1, y1, x2, y2 = box.xyxy[0]
-                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        try:
+            await asyncio.sleep(60 * 1)
+            ret, frame = cap.read()
+            if not ret:
+                break
+            results = m(frame)
+            pople_count = 0
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    if box.cls[0] == 0:
+                        pople_count += 1
+                        x1, y1, x2, y2 = box.xyxy[0]
+                        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
 
-        print(f"人数: {pople_count}")
+            headers = {
+                "Authorization": f"Bearer {os.getenv('API_TOKEN')}",
+                "Content-Type": "application/json"
+            }
+            if pople_count > 0:
+                requests.post('http://localhost:9000/api/webhooks', json={"room_in": True}, headers=headers)
+                zero_count = 0
+            else:
+                if zero_count == 5:
+                    requests.post('http://localhost:9000/api/webhooks', json={"room_in": False}, headers=headers)
+                    zero_count = 0
+                else:
+                    zero_count += 1
+
+            print(f"人数: {pople_count}")
+        except Exception as e:
+            print(f"エラー: {e}")
+            continue
     cap.release()
 
 asyncio.run(detect_video(model, os.getenv('VIDEO_PATH')))
