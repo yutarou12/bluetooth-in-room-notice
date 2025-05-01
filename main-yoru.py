b00dab7fdc18773c0004d243fc07ef7a620811ed
yutarou12 <sasaki.yuutarou12@gmail.com>
2025-05-01 16:52:43 +0900

    fix the first argument must be callable -3

--- main-yoru.py
+++ main-yoru.py
@@ -22,7 +22,7 @@ print(f"モデルがロードされました: {model}")
 cap = cv2.VideoCapture(os.getenv('VIDEO_PATH'))
 
 
-async def detect_video(m, z):
+def detect_video(m, z):
     ret, frame = cap.read()
     if not ret:
         print("no ret")
