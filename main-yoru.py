4b3369c5201c0d07939d5960524dc16a798cafe4
yutarou12 <sasaki.yuutarou12@gmail.com>
2025-05-01 15:56:42 +0900

    fix 60ごとの処理を変更

--- main-yoru.py
+++ main-yoru.py
@@ -22,37 +22,42 @@ async def detect_video(m, video_source):
     zero_count = 0
     while True:
         try:
-            await asyncio.sleep(60 * 1)
-            ret, frame = cap.read()
-            if not ret:
-                break
-            results = m(frame)
-            pople_count = 0
-            for r in results:
-                boxes = r.boxes
-                for box in boxes:
-                    if box.cls[0] == 0:
-                        pople_count += 1
-                        x1, y1, x2, y2 = box.xyxy[0]
-                        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
+            for idx in range(1, frame_count, round(fps*60)):
+                cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
 
-            cv2.imwrite("./tmp/room-img.png", frame)
-            headers = {
-                "Authorization": f"Bearer {os.getenv('API_TOKEN')}",
-                "Content-Type": "application/json"
-            }
-            url = f"http://{os.getenv('API_HOST')}:{os.getenv('API_PORT')}/api/webhooks"
-            if pople_count > 0:
-                requests.post(url, json={"room_in": True}, headers=headers)
-                zero_count = 0
-            else:
-                if zero_count == 5:
-                    requests.post(url, json={"room_in": False}, headers=headers)
+                # await asyncio.sleep(60 * 1)
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                results = m(frame)
+                pople_count = 0
+                for r in results:
+                    boxes = r.boxes
+                    for box in boxes:
+                        if box.cls[0] == 0:
+                            pople_count += 1
+                            x1, y1, x2, y2 = box.xyxy[0]
+                            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+
+                cv2.imwrite("./tmp/room-img.png", frame)
+                headers = {
+                    "Authorization": f"Bearer {os.getenv('API_TOKEN')}",
+                    "Content-Type": "application/json"
+                }
+                url = f"http://{os.getenv('API_HOST')}:{os.getenv('API_PORT')}/api/webhooks"
+                if pople_count > 0:
+                    requests.post(url, json={"room_in": True}, headers=headers)
                     zero_count = 0
                 else:
-                    zero_count += 1
+                    if zero_count == 5:
+                        requests.post(url, json={"room_in": False}, headers=headers)
+                        zero_count = 0
+                    else:
+                        zero_count += 1
 
-            print(f"人数: {pople_count}")
+                print(f"人数: {pople_count}")
         except Exception as e:
             print(f"エラー: {e}")
             continue
